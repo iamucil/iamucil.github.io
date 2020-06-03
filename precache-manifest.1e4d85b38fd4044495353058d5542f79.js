9f311182d837491b20b5be88a6f380523bbc14cf
iamucil <ecko.ucil@gmail.com>
2020-06-03 17:54:01 +0700

    deploy vuetify

---
+++ precache-manifest.1e4d85b38fd4044495353058d5542f79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc169cacd7b30f5db87",
+    "url": "/css/chunk-vendors.a86c35ac.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "93efc925e26999a6d489594864065653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f1ea9acffeab921ac6",
+    "url": "/js/about.d8455908.js"
+  },
+  {
+    "revision": "c27284c6ae58547f4325",
+    "url": "/js/app.a7e723f7.js"
+  },
+  {
+    "revision": "cbc169cacd7b30f5db87",
+    "url": "/js/chunk-vendors.dafc56db.js"
+  },
+  {
+    "revision": "9288047e2ab0db67b59391c5c66bd5fe",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
